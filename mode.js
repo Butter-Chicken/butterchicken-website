2cf849148dedd8f8bbff3a36405dce32705c020e
Benedikt-Gschmeidler <gschmeidler.benedikt@gmail.com>
2021-05-16 11:26:21 +0200

    Darkmode, Textanimation, Form updated
    Changes to darkmode based on device preference
    Added custom JS/CSS textanimation
    Added form backend using google forms

--- mode.js
+++ mode.js
@@ -9,9 +9,28 @@ checkbox.addEventListener('change', function() {
         document.documentElement.setAttribute('data-theme', 'light')
     }
 })
+
+
 let trans = () => {
     document.documentElement.classList.add('transition');
     window.setTimeout(() => {
         document.documentElement.classList.remove('transition');
     }, 1000)
 }
+
+
+
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+        checkbox.checked = true;
+        document.documentElement.setAttribute('data-theme', 'dark')
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light')
+    }
+}
+  
+var x = window.matchMedia("(prefers-color-scheme: dark)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
+
+
--- text.js
+++ text.js
@@ -1,5 +1,58 @@
-//var texts = document.querySelectorAll('u'), i;
-//
-//for (i = 0; i < texts.length; ++i){
-//    texts[i].style.color = "#FFF";
-//}
+var texts = document.querySelectorAll('u.wavy'), i;
+
+
+
+for (i = 0; i < texts.length; ++i){
+
+    const text = texts[i];
+
+    const strText = text.textContent;
+    const splitText = strText.split("");
+    text.textContent = "";
+
+    for (let i = 0; i < splitText.length; i++){
+        text.innerHTML += "<span>" + splitText[i] + "</span>";
+    }
+
+    
+    text.addEventListener("mouseenter", function bounce(){
+        let char = 0;
+        let timer = setInterval(onTick, 50);
+
+        function onTick(){
+            const span = text.querySelectorAll('span')[char];
+            span.classList.add('bounce');
+            char++
+            if(char === splitText.length){
+                complete();
+                return;
+            }
+        }
+
+        function complete(){
+            clearInterval(timer);
+            timer = null;
+        }
+    });
+
+    text.addEventListener("animationend", function stopbounce(){
+        let char = 0;
+        let timer = setInterval(onTick2, 50);
+
+        function onTick2(){
+            const span = text.querySelectorAll('span')[char];
+            span.classList.remove('bounce');
+            char++
+            if(char === splitText.length){
+                complete2();
+                return;
+            }
+        }
+
+        function complete2(){
+            clearInterval(timer);
+            timer = null;
+        }
+    });
+}
+
