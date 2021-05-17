91eabdd691929e67eff8dc9c96e055d79eb9a9fd
Benedikt-Gschmeidler <gschmeidler.benedikt@gmail.com>
2021-05-17 22:06:56 +0200

    Added basic form validation and social media links

--- javascript/text.js
+++ javascript/text.js
@@ -1,6 +1,41 @@
 var texts = document.querySelectorAll('u.wavy'), i;
+var games = document.querySelectorAll('.game'), i;
 
 
+for (i=0; i < games.length; ++i){
+    
+    const game = games[i];
+
+    const text = game.querySelector('u.wavy');
+    const strText = text.textContent;
+    const splitText = strText.split("");
+    text.textContent = "";
+
+    for (let i = 0; i < splitText.length; i++){
+        text.innerHTML += "<span>" + splitText[i] + "</span>";
+    }
+
+    game.addEventListener("mouseenter", function bounceChild(){
+        let char = 0;
+        let timer = setInterval(onTick, 50);
+
+        function onTick(){
+            const span = text.querySelectorAll('span')[char];
+            span.classList.add('bounce');
+            char++
+            if(char === splitText.length){
+                complete();
+                return;
+            }
+        }
+
+        function complete(){
+            clearInterval(timer);
+            timer = null;
+        }
+
+    });
+}
 
 for (i = 0; i < texts.length; ++i){
 
@@ -55,4 +90,3 @@ for (i = 0; i < texts.length; ++i){
         }
     });
 }
-
---
+++ javascript/validate.js
@@ -0,0 +1,67 @@
+// Get the modal
+var successModal = document.getElementById("successModal");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+    resetForm();
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == successModal) {
+        successModal.style.display = "none";
+        resetForm();
+    }
+}
+
+function validateForm(){
+    const form = document.querySelector('.contact-form');
+    var name = form.querySelector('.input-name');
+    var email = form.querySelector('.input-email');
+    var message = form.querySelector('.input-message');
+    var namefail = document.querySelector('.namefail');
+    var emailfail = document.querySelector('.emailfail');
+    var messagefail = document.querySelector('.messagefail');
+    var fail = false;
+    
+    messagefail.textContent = "";
+    emailfail.textContent = "";
+    namefail.textContent = "";
+
+    if (message.value == "") {
+        messagefail.textContent = "🛈 Field cannot be empty";
+        message.select();
+        fail = true;
+    }
+    if (email.value.indexOf("@") <= 0) {
+        emailfail.textContent = "🛈 Please enter a valid email address";
+        email.select();
+        fail = true;
+    }
+    if (email.value == "") {
+        emailfail.textContent = "🛈 Field cannot be empty";
+        email.select();
+        fail = true;
+    }
+    if (name.value == "") {
+        namefail.textContent = "🛈 Field cannot be empty";
+        name.select();
+        fail = true;
+    }
+
+    if (fail){
+        return false;
+    }
+    submitted=true;
+    var successModal = document.getElementById("successModal");
+    successModal.style.display = "block";
+    return true;
+}
+
+function resetForm(){
+    document.getElementById("gform").reset();
+    //document.querySelector('.namefail').textContent = "";
+    //document.querySelector('.emailfail').textContent = "";
+    //document.querySelector('.messagefail').textContent = "";
+}
